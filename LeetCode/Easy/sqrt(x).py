e9bbb59d7bc3704d5312e74c35200f72849afab3
Andrew Virya Victorio <andrew.avv03@gmail.com>
2022-03-21 21:32:42 +0700

    refactor mySqrt function

--- LeetCode/Easy/sqrt(x).py
+++ LeetCode/Easy/sqrt(x).py
@@ -1,5 +1,18 @@
-class Solution(object):
+class Solution(object):      
     def mySqrt(self, number):
-        if number < 0:
-            return
-        return int(number ** .5)
+        left = 1
+        right = number
+        while left <= right:
+            mid = int((left + right) / 2)
+            if number < (mid * mid):
+                right = mid - 1
+            elif number > (mid * mid):
+                left = mid + 1
+            else:
+                return mid
+        return right
+    # Prohibited  
+    # def mySqrt(self, number):
+    #     if number < 0:
+    #         return
+    #     return int(number ** .5)
